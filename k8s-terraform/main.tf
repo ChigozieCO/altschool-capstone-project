a158a7ac8bb6b3c23629b71537264c54489925d0
ChigozieCO <chiggy.nm@gmail.com>
2024-08-09 19:20:55 +0100

    Correct indentations

--- k8s-terraform/main.tf
+++ k8s-terraform/main.tf
@@ -31,7 +31,7 @@ provider "kubernetes" {
   host = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
   }
@@ -42,7 +42,7 @@ provider "kubectl" {
   host = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
   }
@@ -54,7 +54,7 @@ provider "helm" {
     host = var.cluster_endpoint
     cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
+      api_version = "client.authentication.k8s.io/v1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
     }
