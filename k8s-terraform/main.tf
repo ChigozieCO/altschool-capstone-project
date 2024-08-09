4bea70558c8fcdfff84dd82b6baa579dd8bc9b4e
ChigozieCO <chiggy.nm@gmail.com>
2024-08-09 19:29:17 +0100

    Correct kubectl provider configuration

--- k8s-terraform/main.tf
+++ k8s-terraform/main.tf
@@ -42,6 +42,7 @@ provider "kubectl" {
   host = var.cluster_endpoint
   cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
   exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
   }
